2c2014aa01c8223229d175cb73c57ca32739721a
Takaya Fukuda <efutty@MBA20.local>
2020-06-28 21:08:46 +0900

    🌱 初回コミット

---
+++ src/test/java/jp/co/sample/demo/testng/app/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package jp.co.sample.demo.testng.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
